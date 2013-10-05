7002bf86f0de969380e70421acaa3f9e5354d369
TungstenNo74 <ctwimber@gmail.com>
2013-10-05 16:46:27 -0500

    got scorecomparer.py and window.py closer to v0.2 spec.

--- redo.py
+++ redo.py
@@ -39,12 +39,52 @@ class Main_Window(QtGui.QWidget):
     def open_score_frame(self):
         self.score_frame = Score_Frame()
         self.score_frame.sub_btn.clicked.connect(self.append_data)
+        self.score_frame.rst_btn.clicked.connect(self.reset_text)
     
     def append_data(self):
+
         scores_string = str(self.score_frame.score_entry_box.toPlainText())
-        punc_array = [',', ';']
+
+        """"I'm using the array below to hold any punctuation marks I want to 
+        remove.  It's not the most readable way of doing this, but I want to
+        be able to add and remove marks easily."""
+        punc_array = [',', ';'] 
         for punc in punc_array:
             scores_string = scores_string.replace(punc, ' ')
+
+        scores_array = scores_string.split()
+        cleaned_array = []
+        not_accepted = 0
+        not_accepted_array = []
+        for score in scores_array:
+            if score.replace('.', '').replace('-','').isdigit():
+                cleaned_array.append(float(score))
+            else:
+                not_accepted += 1
+                not_accepted_array.append(score)
+
+        if not_accepted > 0:
+            "Tell the user which scores didn't make it in"
+            QtGui.QMessageBox.about(self,
+                                    'Invalid Scores',
+                                    'Not all scores were accepted: %s'
+                                    % ', '.join(not_accepted_array))
+                
+        scores_dict = {}
+        scores_dict['scores'] = cleaned_array
+        
+        
+        #Insert the date
+        date = self.score_frame.cal.selectedDate()
+        scores_dict['date'] = date
+        self.score_frame.close()
+
+        self.data_array.append(scores_dict)
+        print self.data_array[len(self.data_array)-1]['date']
+        print self.data_array[len(self.data_array)-1]['scores']
+    
+    def reset_text(self):
+        self.score_frame.score_entry_box.setText('')
 
 class Score_Frame(QtGui.QWidget):
     """
@@ -59,7 +99,20 @@ class Score_Frame(QtGui.QWidget):
     def initUi(self):
         """The Ui layout.  All buttons and widgets need to be 'selfed', but
         let's leave the methods out of this thing..."""
-        
+
+        wndw_box = QtGui.QVBoxLayout()
+
+        #Calendar
+        wndw_box.addWidget(QtGui.QLabel("Enter the date of the Exam"))
+        self.cal = QtGui.QCalendarWidget()
+        wndw_box.addWidget(self.cal)
+
+        #Score Entry Box
+        wndw_box.addWidget(QtGui.QLabel("Enter Scores Below"))
+        self.score_entry_box = QtGui.QTextEdit()
+        wndw_box.addWidget(self.score_entry_box)
+
+        #Buttons
         btn_box = QtGui.QHBoxLayout()
         btn_box.addStretch(1)
 
@@ -70,12 +123,6 @@ class Score_Frame(QtGui.QWidget):
         btn_box.addWidget(self.sub_btn)
         btn_box.addWidget(self.ccl_btn)
         btn_box.addWidget(self.rst_btn)
-
-        wndw_box = QtGui.QVBoxLayout()
-        wndw_box.addWidget(QtGui.QLabel("Enter Scores Below"))
-        
-        self.score_entry_box = QtGui.QTextEdit()
-        wndw_box.addWidget(self.score_entry_box)
         wndw_box.addLayout(btn_box)
         
         self.setLayout(wndw_box)
